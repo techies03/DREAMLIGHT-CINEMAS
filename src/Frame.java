8bcb8f8de4354afdb5c22cdc9ada4c1614030eb2
Ceed03 <mrmuqsit2003@gmail.com>
2022-06-22 20:56:36 +0800

    REMOVE MAIN METHOD

--- src/Frame.java
+++ src/Frame.java
@@ -17,18 +17,6 @@ public class Frame extends JFrame {
 	private JTextField txtUsername;
 	private JPasswordField txtPassword;
 
-
-	public static void main(String[] args) {
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				Frame frame = new Frame();
-				frame.setVisible(true);
-
-			}
-		});
-	}
-
 	//create class for Frame
 	public Frame() {
 		
